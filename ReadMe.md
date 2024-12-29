ed493366cc00e9297dd1bf8b29b1c31971928cb4
caffidev <caffidev@gmail.com>
2024-12-30 02:27:48 +1000

    feat: added some properties

--- ReadMe.md
+++ ReadMe.md
@@ -1,2 +1,2 @@
-# ETS 2 save editor
-So when I played ETS 2, I have found that both of save game editor projects are unmaintained, so I decided to write my own.
+# File Analysis Tool
+This app was written to analyze files, and give as much info about it as possible
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,42 @@
-use std::{fs::File, io::{BufRead, BufReader}, path::PathBuf};
+extern crate bytesize;
 
+use std::{env, fs, time::SystemTime};
+use clap::{Arg, ArgAction, Command};
 use fltk::{app, button::Button, frame::Frame, prelude::*, window::Window};
-
-fn is_empty_and_readable(path: &PathBuf) -> bool {
-    path.read_dir().map(|mut i| i.next().is_none()).unwrap_or(false)
-}
+use bytesize::ByteSize;
+use time::OffsetDateTime;
 fn main() {
+    let m = Command::new("File Analysis Tool")
+        .author("caffidev, caffidev@gmail.com")
+        .version("0.1.1")
+        .about("FAT - File Analysis Tool, analyzes file, tries to guess its extension etc..")
+        .disable_help_subcommand(true)
+        .disable_help_flag(true)
+        .arg(
+            Arg::new("debug")
+                .action(ArgAction::SetTrue)
+                .short('d')
+                .long("debug")
+                .help("Turns on debugging mode")
+        )
+        // Change help short flag to `?`
+        .arg(
+            Arg::new("help")
+                .short('?')
+                .long("help")
+                .action(ArgAction::Help)
+                .help("Print help")
+        )
+        .arg(
+            Arg::new("human")
+            .action(ArgAction::SetTrue)
+                .short('h')
+                .long("human")
+                .help("Prints numbers in human-readable way (124K, 76M)")
+        )
+        .after_help("This app was written to analyze files, and give as much info about it as possible")
+        .get_matches();
+
     let app = app::App::default();
     let mut wind = Window::new(100, 100, 400, 300, "ETS2 save editor v0.1.1");
     let mut frame = Frame::new(0, 0, 400, 200, "");
@@ -13,53 +44,24 @@ fn main() {
     wind.end();
     wind.show();
     but.set_callback(move |_| frame.set_label("Hello World!")); // the closure capture is mutable borrow to our button
-    
-    let mut path_to_profiles : PathBuf = Default::default();
-    // Start of ETS2 save editor.
-    // For now supports only windows
-    match dirs_next::document_dir() {
-        Some(path) => {
-            path.clone_into(&mut path_to_profiles);
-        },
-        None => {
-            println!("Couldn't find a documents folder in your system. Exiting app...");
-            // TODO: proper handling for app.quit()
-            app.quit();
-        }
-    }
-    
-    path_to_profiles.push("Euro Truck Simulator 2");
-    path_to_profiles.push("profiles");
+    let mut zip_path = env::current_dir().unwrap();
+    zip_path.push(r"test_files\test.zip");
+    println!("{:?}",zip_path);
+    if zip_path.exists() && zip_path.is_file() {
+        print!("{} - ",zip_path.file_name().unwrap().to_string_lossy());
+        let metadata = fs::metadata(zip_path).unwrap();
 
-    if !path_to_profiles.exists() {
-        println!("Path to profiles does not exist/is not accessible. Please launch ETS 2 at least once.");
-        app.quit();
-    }
-
-    // Checking if directory readable & empty
-    if is_empty_and_readable(&path_to_profiles) {
-        println!("Couldn't find any profiles, although profile folder exists. Trying to check steam cloud saves...");
+        if !m.get_flag("human") {println!("{:?}", metadata.len())}
+        else { print!("{} - ",ByteSize(metadata.len()).to_string_as(true));}
+        // TODO: proper handling of inaccessible time
+        let created_time : OffsetDateTime = metadata.created().unwrap_or(SystemTime::now()).into();
+        let accessed_time : OffsetDateTime = metadata.accessed().unwrap_or(SystemTime::now()).into();
+        print!("created: {:0>4}-{:0>2}-{:0>2} {:0>2}:{:0>2}:{:0>2} - ", created_time.year(), created_time.month() as u8, created_time.day(), created_time.hour(), created_time.minute(), created_time.second());
+        print!("last accessed: {:0>4}-{:0>2}-{:0>2} {:0>2}:{:0>2}:{:0>2} - ", accessed_time.year(), accessed_time.month() as u8, accessed_time.day(), accessed_time.hour(), accessed_time.minute(), accessed_time.second());
         
-        // TODO: properly 
-        path_to_profiles = PathBuf::from(r"C:\Program Files (x86)\Steam\userdata\362100980\227300\remote\profiles\");
-        // TODO: errors if unreadable, need a proper way to handle
-        if is_empty_and_readable(&path_to_profiles) {
-            println!("Path to profiles does not exist/is not accessible. Please launch ETS 2 at least once.");
-            app.quit();
-        }
+        if metadata.permissions().readonly() { println!("readonly");} 
+        else { println!("read&writeable");}
     }
-    // My magic profile name
-    path_to_profiles.push(r"D181616666696465765F6D6F62696C65");
-    // My magically decrypted profile.sii
-    path_to_profiles.push("profile.sii2");
-    let file = File::open(path_to_profiles).unwrap();
-    let buf_reader = BufReader::new(file);
-    let mut lines = buf_reader.lines();
-    let next_line = lines.next().unwrap().unwrap();
-    if next_line == "SiiNunit" {
-        println!("File provided is correct");
-    }
+
     app.run().unwrap();
-
-
 }
